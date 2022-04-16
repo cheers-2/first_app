fcaf6f21d6ec97dd17916e0819919866989df4b6
hecate021196 <harryang60@gmail.com>
2022-04-16 09:01:43 +0800

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answeredasdasd');
+  }
+
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    var questions = ['what\'s is my name', 'what\'s is your name'];
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('First App'),
+          ),
+          body: Column(
+            children: [
+              Text('Question'),
+              RaisedButton(
+                child: Text('Answer 1'),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text('Answer 2'),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text('Answer 3'),
+                onPressed: answerQuestion,
+              ),
+            ],
+          )),
+    );
+  }
+}
